d34c68058a5d5b48eae090d7f116170e83b41c1f
talarKoudoussian <talar.koudoussian@gmail.com>
2018-05-21 13:31:09 +0300

    chore: modify api to return status code with employee object in json format

--- src/main/java/employee/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package employee;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-
-}
--- src/main/java/employee/data/Employee.java
+++ src/main/java/employee/data/Employee.java
@@ -1,8 +1,6 @@
-package employee;
+package employee.data;
 
 import javax.persistence.*;
-//import java.text.SimpleDateFormat;
-//import java.util.Date;
 
 @Entity
 public class Employee {
@@ -11,16 +9,20 @@ public class Employee {
     @GeneratedValue(strategy=GenerationType.AUTO)
     @Column(name="employee_id")
     private long employeeId;
+
     @Column(name="first_name")
     private String firstName;
+
     @Column(name="last_name")
     private String lastName;
+
     @Column(name="added_date")
     private String addedDate;
+
     @Column(name="employment_status")
     private boolean employmentStatus;
 
-    public long getEmployeeId() {
+    public Long getEmployeeId() {
         return employeeId;
     }
 
@@ -52,11 +54,11 @@ public class Employee {
         this.addedDate = addedDate;
     }
 
-    public Boolean getStatus() {
+    public boolean getEmploymentStatus() {
         return employmentStatus;
     }
 
-    public void setStatus(boolean employmentStatus) {
+    public void setEmploymentStatus(boolean employmentStatus) {
         this.employmentStatus = employmentStatus;
     }
 
@@ -64,14 +66,14 @@ public class Employee {
     public String toString(){
         StringBuilder sb = new StringBuilder();
         sb.append("Employee[firstName: ");
-        sb.append(this.firstName);
-        sb.append(" lastName: ");
-        sb.append( this.lastName);
-        sb.append(" addedDate: ");
-        sb.append(this.addedDate);
-        sb.append(" employmentStatus: ");
-        sb.append(this.employmentStatus);
-        sb.append( " ]");
+        sb.append(firstName);
+        sb.append(", lastName: ");
+        sb.append(lastName);
+        sb.append(", addedDate: ");
+        sb.append(addedDate);
+        sb.append(", employmentStatus: ");
+        sb.append(employmentStatus);
+        sb.append("]");
         return sb.toString();
     }
 }
