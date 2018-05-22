c710214f0fa6328ab7b455fd47d01cbb694d6b09
talarKoudoussian <talar.koudoussian@gmail.com>
2018-05-22 09:52:15 +0300

    feat: modify api to save employee to database

--- src/main/java/employee/controller/EmployeeController.java
+++ src/main/java/employee/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package employee.controller;
 
 import employee.data.Employee;
+import employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,16 +10,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class EmployeeController {
 
-//    @Autowired
-//    EmployeeRepository employeeRepository;
+    @Autowired
+    EmployeeRepository employeeRepository;
 
     @ResponseBody
     @RequestMapping(method = RequestMethod.POST, value = "/employees", produces = "application/json;charset=UTF-8")
     public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
         System.out.println(employee.toString());
-//        employeeRepository.save(employee);
-//        return employee.getEmployeeId();
-        employee.setEmployeeId(100);
-        return new ResponseEntity<>(employee, HttpStatus.CREATED);
+        Employee newEmployee = employeeRepository.save(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
     }
 }
--- src/main/java/employee/repository/EmployeeRepository.java
+++ src/main/java/employee/repository/EmployeeRepository.java
@@ -1,7 +1,10 @@
-//package employee;
-//
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-//
-//}
+package employee.repository;
+
+import employee.data.Employee;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
